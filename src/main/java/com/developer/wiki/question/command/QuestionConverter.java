cdd15077b785aecf9f796db869038a178ecefb60
dhkstnaos <dhkstnaos@gmail.com>
2022-09-27 17:46:47 +0900

    feat: converter 추가

---
+++ src/main/java/com/developer/wiki/question/command/QuestionConverter.java
@@ -0,0 +1,13 @@
+package com.developer.wiki.question.command;
+
+import com.developer.wiki.question.command.application.CreateQuestionRequest;
+import com.developer.wiki.question.command.domain.Question;
+
+public class QuestionConverter {
+
+  public static Question toQuestion(CreateQuestionRequest createQuestionRequest) {
+    return new Question(createQuestionRequest.getTitle(), createQuestionRequest.getNickname(),
+        PasswordEncrypter.encrypt(createQuestionRequest.getPassword()),
+        createQuestionRequest.getCategory(), createQuestionRequest.getAdditionQuestions());
+  }
+}
