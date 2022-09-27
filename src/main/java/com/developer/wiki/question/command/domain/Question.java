eace66e79286ff6031f67ed9acae4c1930d14b7a
dhkstnaos <dhkstnaos@gmail.com>
2022-09-27 17:33:02 +0900

    feat: Question 엔티티 생성

---
+++ src/main/java/com/developer/wiki/question/command/domain/Question.java
@@ -0,0 +1,58 @@
+package com.developer.wiki.question.command.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "question")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "nickname")
+  private String nickname;
+
+  @Column(name = "password")
+  private String password;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "category")
+  private Category category = Category.없음;
+
+  @ElementCollection(fetch = FetchType.LAZY)
+  private List<String> additionQuestions = new ArrayList<>();
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  public Question(String title, String nickname, String password, Category category,
+      List<String> additionQuestions) {
+    this.title = title;
+    this.nickname = nickname;
+    this.password = password;
+    this.category = category;
+    this.additionQuestions = additionQuestions;
+    this.createdAt = LocalDateTime.now();
+  }
+}
