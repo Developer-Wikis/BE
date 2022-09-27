bf81118c7ada35948f3f44ff3929530d735959fa
dhkstnaos <dhkstnaos@gmail.com>
2022-09-27 17:52:13 +0900

    feat: 질문 등록 controller

---
+++ src/main/java/com/developer/wiki/question/presentation/QuestionCreateController.java
@@ -0,0 +1,24 @@
+package com.developer.wiki.question.presentation;
+
+import com.developer.wiki.question.command.application.CreateQuestionRequest;
+import com.developer.wiki.question.command.application.QuestionCreateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/questions")
+@RequiredArgsConstructor
+public class QuestionCreateController {
+
+  private final QuestionCreateService questionCreateService;
+
+  @PostMapping
+  public ResponseEntity<Long> create(@RequestBody CreateQuestionRequest createQuestionRequest) {
+    Long id = questionCreateService.questionCreate(createQuestionRequest);
+    return ResponseEntity.ok(id);
+  }
+}
