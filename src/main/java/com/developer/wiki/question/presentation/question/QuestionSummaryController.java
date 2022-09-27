6dcc28c28c7d8d1a7719ac5838d968c45475f6a0
dhkstnaos <dhkstnaos@gmail.com>
2022-09-27 20:18:45 +0900

    feat: 질문 목록 조회

---
+++ src/main/java/com/developer/wiki/question/presentation/question/QuestionSummaryController.java
@@ -0,0 +1,25 @@
+package com.developer.wiki.question.presentation.question;
+
+import com.developer.wiki.question.query.application.QuestionSummaryService;
+import com.developer.wiki.question.query.application.SummaryQuestionResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/questions")
+@RequiredArgsConstructor
+public class QuestionSummaryController {
+
+  private final QuestionSummaryService questionSummaryService;
+
+  @PostMapping
+  public ResponseEntity<Slice<SummaryQuestionResponse>> getSlice(Pageable pageable) {
+    Slice<SummaryQuestionResponse> summarySlice = questionSummaryService.findSlice(pageable);
+    return ResponseEntity.ok(summarySlice);
+  }
+}
--- src/main/java/com/developer/wiki/question/util/QuestionConverter.java
+++ src/main/java/com/developer/wiki/question/util/QuestionConverter.java
@@ -2,6 +2,7 @@ package com.developer.wiki.question.util;
 
 import com.developer.wiki.question.command.application.dto.CreateQuestionRequest;
 import com.developer.wiki.question.command.domain.Question;
+import com.developer.wiki.question.query.application.SummaryQuestionResponse;
 
 public class QuestionConverter {
 
@@ -9,5 +10,12 @@ public class QuestionConverter {
     return new Question(createQuestionRequest.getTitle(), createQuestionRequest.getNickname(),
         PasswordEncrypter.encrypt(createQuestionRequest.getPassword()),
         createQuestionRequest.getCategory(), createQuestionRequest.getAdditionQuestions());
+  }
+
+  public static SummaryQuestionResponse ofSummary(Question question) {
+    return SummaryQuestionResponse.builder().id(question.getId()).title(question.getTitle())
+        .nickname(question.getNickname()).category(question.getCategory())
+        .viewCount(question.getViewCount()).commentCount(question.getCommentCount())
+        .createdAt(question.getCreatedAt()).build();
   }
 }
