a9e5d703ffafb090f9acb5c366416b49a9732a0b
dhkstnaos <dhkstnaos@gmail.com>
2022-09-27 17:50:03 +0900

    feat: 질문 등록 service 생성

--- src/main/java/com/developer/wiki/question/command/application/QuestionCreateService.java
+++ src/main/java/com/developer/wiki/question/command/application/QuestionCreateService.java
@@ -1,2 +1,21 @@
-package com.developer.wiki.question.command.application;public class QuestionCreate {
+package com.developer.wiki.question.command.application;
+
+import com.developer.wiki.question.command.QuestionConverter;
+import com.developer.wiki.question.command.domain.Question;
+import com.developer.wiki.question.command.domain.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class QuestionCreateService {
+
+  private final QuestionRepository questionRepository;
+
+  public Long questionCreate(CreateQuestionRequest createQuestionRequest) {
+    Question question = QuestionConverter.toQuestion(createQuestionRequest);
+    return questionRepository.save(question).getId();
+  }
 }
