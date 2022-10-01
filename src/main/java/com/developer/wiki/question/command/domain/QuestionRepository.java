897d7228759a86ae71bb6514a2e5d3ce28e69196
dhkstnaos <dhkstnaos@gmail.com>
2022-10-02 01:26:19 +0900

    feat: searchRepo

--- src/main/java/com/developer/wiki/question/command/domain/QuestionRepository.java
+++ src/main/java/com/developer/wiki/question/command/domain/QuestionRepository.java
@@ -1,14 +1,13 @@
 package com.developer.wiki.question.command.domain;
 
 import java.util.Optional;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-  Slice<Question> findSliceBy(Pageable pageable);
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long>{
 
-  @Query("SELECT distinct q FROM Question q join fetch q.comments")
+  @Query("SELECT distinct q FROM Question q left join fetch q.comments where q.id = :questionId")
   Optional<Question> findDetail(Long questionId);
 }
--- src/main/java/com/developer/wiki/question/command/domain/QuestionSearchRepository.java
+++ src/main/java/com/developer/wiki/question/command/domain/QuestionSearchRepository.java
@@ -3,7 +3,7 @@ package com.developer.wiki.question.command.domain;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
-public interface QuestionRepositoryCustom {
+public interface QuestionSearchRepository {
 
   Slice<Question> findSliceBy(Pageable pageable, Category category);
 }
