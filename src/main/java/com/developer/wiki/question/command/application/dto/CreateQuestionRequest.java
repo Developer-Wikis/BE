3c911a664f8bfc5cb66bd7bab75328749cf35fba
dhkstnaos <dhkstnaos@gmail.com>
2022-10-02 01:26:56 +0900

    feat: dto valid 추가

--- src/main/java/com/developer/wiki/question/command/application/dto/CreateQuestionRequest.java
+++ src/main/java/com/developer/wiki/question/command/application/dto/CreateQuestionRequest.java
@@ -3,6 +3,7 @@ package com.developer.wiki.question.command.application.dto;
 import com.developer.wiki.question.command.domain.Category;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +15,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CreateQuestionRequest {
 
+  @NotBlank(message = "닉네임은 null일 수 없습니다.")
   private String nickname;
+  @NotBlank(message = "비밀번호는 null일 수 없습니다.")
   private String password;
+  @NotBlank(message = "제목은 null일 수 없습니다.")
   private String title;
+  @CategoryValid
   private Category category;
   @Builder.Default
   private List<String> additionQuestions = new ArrayList<>();
