5e30b9dd363aa6de1a8d926346e48b55f672ae3e
dhkstnaos <dhkstnaos@gmail.com>
2022-10-03 23:22:10 +0900

    feat: 상세조회 이전 다음 질문, 이전 페이지에서 정보를 받아오도록 수정

--- src/main/java/com/developer/wiki/question/command/domain/Category.java
+++ src/main/java/com/developer/wiki/question/command/domain/Category.java
@@ -12,8 +12,9 @@ public enum Category {
 
   private final String category;
 
-  public String getCategory() {
-    return category;
+  public static List<Category> frontendAll() {
+    return List.of(Category.FE기본, Category.CSS, Category.HTML,  Category.JAVASCRIPT,
+        Category.REACT, Category.네트워크, Category.디자인패턴, Category.보안, Category.자료구조_알고리즘);
   }
 
   public static Category of(String category) {
@@ -25,9 +26,8 @@ public enum Category {
     return null;
   }
 
-  public static List<Category> frontendAll() {
-    return List.of(Category.FE기본, Category.CSS, Category.HTML,  Category.JAVASCRIPT,
-        Category.REACT, Category.네트워크, Category.디자인패턴, Category.보안, Category.자료구조_알고리즘);
+  public String getCategory() {
+    return category;
   }
 
   public static List<Category> backendAll() {
--- src/main/java/com/developer/wiki/question/command/domain/QuestionRepository.java
+++ src/main/java/com/developer/wiki/question/command/domain/QuestionRepository.java
@@ -1,5 +1,6 @@
 package com.developer.wiki.question.command.domain;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,16 +12,14 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
   @Query("SELECT distinct q FROM Question q left join fetch q.comments left join fetch q.additionQuestions where q.id = :questionId")
   Optional<Question> findDetail(Long questionId);
 
-  @Query(value =
-      "select query.prevId "
-          + "from (select id,LAG(id,1) over(partition by q.category order by created_at) as prevId from question as q order by q.created_at) as query "
-          + "where query.id = :questionId", nativeQuery = true)
-  Optional<Long> findPrevIdById(Long questionId);
+  @Query(value = "select query.prevId "
+      + "from (select id,LAG(id,1) over(partition by q.category in :categories order by created_at) as prevId from question as q order by q.created_at) as query "
+      + "where query.id = :questionId", nativeQuery = true)
+  Optional<Long> findPrevIdById(Long questionId, List<String> categories);
 
-  @Query(value =
-      "select query.nextId "
-          + "from (select id,LEAD(id,1) over(partition by q.category order by created_at) as nextId from question as q order by q.created_at) as query "
-          + "where query.id = :questionId", nativeQuery = true)
-  Optional<Long> findNextIdById(Long questionId);
+  @Query(value = "select query.nextId "
+      + "from (select id,LEAD(id,1) over(partition by q.category in :categories order by created_at) as nextId from question as q order by q.created_at) as query "
+      + "where query.id = :questionId", nativeQuery = true)
+  Optional<Long> findNextIdById(Long questionId, List<String> categories);
 
 }
