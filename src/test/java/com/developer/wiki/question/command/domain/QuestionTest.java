48f2f37a3b6f2e0203618b0a132e031ff3b74e3d
dhkstnaos <dhkstnaos@gmail.com>
2022-10-04 13:05:43 +0900

    feat: question 생성 테스트

---
+++ src/test/java/com/developer/wiki/question/command/domain/QuestionTest.java
@@ -0,0 +1,23 @@
+package com.developer.wiki.question.command.domain;
+
+import com.developer.wiki.question.util.PasswordEncrypter;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionTest {
+
+  @Autowired
+  QuestionRepository questionRepository;
+
+  @Test
+  void Question_create() {
+    Question question = new Question("title", "nickname", PasswordEncrypter.encrypt("234"),
+        Category.CSS, List.of("sdf", "SDfdf"));
+    Question savedQuestion = questionRepository.save(question);
+    Assertions.assertThat(savedQuestion.getId()).isNotNull();
+  }
+}
