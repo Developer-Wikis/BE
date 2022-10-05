600ad5e5cba8561c891f8cde47db505e148c0d9f
jhdl0157 <jhdl0157@naver.com>
2022-10-05 11:25:42 +0700

    Fix : 카테고리 수정

--- src/main/java/com/developer/wiki/question/command/domain/Category.java
+++ src/main/java/com/developer/wiki/question/command/domain/Category.java
@@ -8,11 +8,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public enum Category {
-    fe_all, be_all, fe_basic, be_basic, css, html, javascript, react, java,
-    spring, network, database, design_pattern, security, os, data_structure_algorithm,
-    infra_engineering;
+    fe_all("fe all"), be_all("de all"), fe_basic("fe_basic"), be_basic("be_basic")
+  , css("css"), html("html"), javascript("javascript"), react("react"), java("java"),
+    spring("spring"), network("network"), database("database"), design_pattern("design_pattern")
+  , security("security"), os("os"), data_structure_algorithm("data_structure/algorithm"),
+    infra_engineering("infra/engineering");
 
-//  private final String category;
+   private final String category;
 
   public static List<Category> frontendAll() {
     return List.of(Category.fe_basic, Category.css, Category.html, Category.javascript, Category.react,
@@ -24,17 +26,16 @@ public enum Category {
       return null;
     }
     for (Category c : Category.values()) {
-      if (c.toString().equals(category)) {
+      if (c.category.equals(category.toLowerCase())) {
         return c;
       }
     }
-    log.info("이건 테스트 {}",category);
     throw new NotMatchCategoryException();
   }
 
-//  public String getCategory() {
-//    return category;
-//  }
+  public String getCategory() {
+    return category;
+  }
 
   public static List<Category> backendAll() {
     return List.of(Category.be_basic, Category.java, Category.spring, Category.database, Category.os,
