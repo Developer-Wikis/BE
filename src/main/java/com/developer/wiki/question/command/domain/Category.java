6b1982f6b75c09e155ef9ced35cba3640aa14327
jhdl0157 <jhdl0157@naver.com>
2022-10-07 01:11:54 +0700

    카테고리 변경(fe,be)

--- src/main/java/com/developer/wiki/question/command/domain/Category.java
+++ src/main/java/com/developer/wiki/question/command/domain/Category.java
@@ -8,17 +8,18 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public enum Category {
-    fe_all("fe all"), be_all("de all"), fe_basic("fe_basic"), be_basic("be_basic")
-  , css("css"), html("html"), javascript("javascript"), react("react"), java("java"),
-    spring("spring"), network("network"), database("database"), design_pattern("design_pattern")
-  , security("security"), os("os"), data_structure_algorithm("data_structure/algorithm"),
-    infra_engineering("infra/engineering");
+  fe_all("fe all"), fe_basic("fe basic"), fe_css("fe css"), fe_html("fe html"), fe_javascript("fe javascript"), fe_react("fe react"),
+  fe_database("fe database"), fe_design_pattern("fe design pattern"), fe_security_network("fe network/security"), fe_os("fe os"),
+  fe_data_structure_algorithm("fe data structure/algorithm"), fe_infra_engineering("fe infra/engineering"),
+  be_all("be all"), be_java("be java"), be_basic("be basic"),
+  be_spring("be spring"), be_database("be database"), be_design_pattern("be design pattern"), be_security_network("be network/security"), be_os("be os"),
+  be_data_structure_algorithm("be data structure/algorithm"), be_infra_engineering("be infra/engineering");
 
    private final String category;
 
   public static List<Category> frontendAll() {
-    return List.of(Category.fe_basic, Category.css, Category.html, Category.javascript, Category.react,
-        Category.network, Category.design_pattern, Category.security, Category.data_structure_algorithm);
+    return List.of(Category.fe_basic, Category.fe_css, Category.fe_html, Category.fe_javascript, Category.fe_react
+        , Category.fe_design_pattern, Category.fe_security_network, Category.fe_data_structure_algorithm);
   }
 
   public static Category of(String category) {
@@ -38,7 +39,7 @@ public enum Category {
   }
 
   public static List<Category> backendAll() {
-    return List.of(Category.be_basic, Category.java, Category.spring, Category.database, Category.os,
-        Category.infra_engineering, Category.network, Category.design_pattern, Category.security, Category.data_structure_algorithm);
+    return List.of(Category.be_basic, Category.be_java, Category.be_spring, Category.be_database, Category.be_os,
+        Category.be_infra_engineering, Category.be_design_pattern, Category.be_security_network, Category.be_data_structure_algorithm);
   }
 }
