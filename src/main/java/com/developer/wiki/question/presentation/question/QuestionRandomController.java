9289bb3b9e3d9db0854db41631370c1b3a387b77
dhkstnaos <dhkstnaos@gmail.com>
2022-10-16 23:58:18 +0900

    feat: 랜덤 컨틀로러 추가

--- src/main/java/com/developer/wiki/question/presentation/question/QuestionRandomController.java
+++ src/main/java/com/developer/wiki/question/presentation/question/QuestionRandomController.java
@@ -1,5 +1,12 @@
 package com.developer.wiki.question.presentation.question;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/questions")
+@RequiredArgsConstructor
 public class QuestionRandomController {
 
 }
