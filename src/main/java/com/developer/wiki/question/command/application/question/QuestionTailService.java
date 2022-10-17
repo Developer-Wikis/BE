baac90f1374dfef811fab224704d53339ad45616
dhkstnaos <dhkstnaos@gmail.com>
2022-10-17 15:29:35 +0900

    feat: 질문 상세 조회 시 승인 안된 꼬리 질문은 제외하라

--- src/main/java/com/developer/wiki/question/command/application/question/QuestionTailService.java
+++ src/main/java/com/developer/wiki/question/command/application/question/QuestionTailService.java
@@ -1,5 +1,11 @@
 package com.developer.wiki.question.command.application.question;
 
+import com.developer.wiki.question.command.application.dto.CreateTailQuestionRequest;
+import com.developer.wiki.question.command.domain.EntityNotFoundException;
+import com.developer.wiki.question.command.domain.Question;
+import com.developer.wiki.question.command.domain.QuestionRepository;
+import com.developer.wiki.question.command.domain.TailQuestion;
+import com.developer.wiki.question.command.domain.TailQuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,7 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class QuestionTailService {
 
-  public Long tailCreate(String tailQuestion) {
-    return 1L;
+  private final QuestionRepository questionRepository;
+  private final TailQuestionRepository tailQuestionRepository;
+
+  public Long tailCreate(Long questionId, CreateTailQuestionRequest tailQuestion) {
+    Question question = questionRepository.findById(questionId)
+        .orElseThrow(EntityNotFoundException::new);
+    TailQuestion tail = tailQuestionRepository.save(
+        new TailQuestion(tailQuestion.getTailQuestion(), question));
+    return tail.getId();
   }
 }
--- src/main/java/com/developer/wiki/question/presentation/question/QuestionTailController.java
+++ src/main/java/com/developer/wiki/question/presentation/question/QuestionTailController.java
@@ -1,8 +1,10 @@
 package com.developer.wiki.question.presentation.question;
 
+import com.developer.wiki.question.command.application.dto.CreateTailQuestionRequest;
 import com.developer.wiki.question.command.application.question.QuestionTailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,9 @@ public class QuestionTailController {
   private final QuestionTailService questionTailService;
 
   @PostMapping("/{questionId}/tail")
-  public ResponseEntity<Long> tailCreate(@RequestBody String tailQuestion) {
-    Long id = questionTailService.tailCreate(tailQuestion);
+  public ResponseEntity<Long> tailCreate(@PathVariable Long questionId,
+      @RequestBody CreateTailQuestionRequest tailQuestion) {
+    Long id = questionTailService.tailCreate(questionId, tailQuestion);
     return ResponseEntity.ok(id);
   }
 }
