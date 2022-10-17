450ec4fabdaf38a5502d6f5ee28fbfa40a1b213a
dhkstnaos <dhkstnaos@gmail.com>
2022-10-17 13:59:29 +0900

    feat: 레포 의존성 변경

--- src/main/java/com/developer/wiki/question/command/domain/QuestionRepository.java
+++ src/main/java/com/developer/wiki/question/command/domain/QuestionRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+public interface QuestionRepository extends JpaRepository<Question, Long>{
 
   @Query("SELECT distinct q FROM Question q left join fetch q.comments left join fetch q.additionQuestions where q.id = :questionId")
   Optional<Question> findDetail(Long questionId);
@@ -15,11 +15,11 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
   @Query(value = "select query.prevId "
       + "from (select id,LAG(id,1) over(partition by q.sub_category in :subCategory order by created_at) as prevId from question as q where q.is_approved is true and q.main_category =:mainCategory order by q.created_at) as query "
       + "where query.id = :questionId", nativeQuery = true)
-  Optional<Long> findPrevIdById(Long questionId,String mainCategory, List<String> subCategory);
+  Optional<Long> findPrevIdById(Long questionId, String mainCategory, List<String> subCategory);
 
   @Query(value = "select query.nextId "
       + "from (select id,LEAD(id,1) over(partition by q.sub_category in :subCategory order by created_at) as nextId from question as q where q.is_approved is true and q.main_category =:mainCategory order by q.created_at) as query "
       + "where query.id = :questionId", nativeQuery = true)
-  Optional<Long> findNextIdById(Long questionId,String mainCategory, List<String> subCategory);
+  Optional<Long> findNextIdById(Long questionId, String mainCategory, List<String> subCategory);
 
 }
