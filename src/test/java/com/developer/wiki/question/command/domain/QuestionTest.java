977e543adf3f5b6d41f4b00556c14c5925d0edea
jhdl0157 <jhdl0157@naver.com>
2022-10-23 15:50:04 +0700

    Fix :Test 주석처리

--- src/test/java/com/developer/wiki/question/command/domain/QuestionTest.java
+++ src/test/java/com/developer/wiki/question/command/domain/QuestionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class QuestionTest {
 
   @Autowired
