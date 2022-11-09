28b44d9a05cf3f6f52d504ef83b5a0b278ed5c55
jhdl0157 <jhdl0157@naver.com>
2022-11-09 16:33:31 +0900

    Fix : cors 설정 변경

--- src/main/java/com/developer/wiki/config/CorsConfig.java
+++ src/main/java/com/developer/wiki/config/CorsConfig.java
@@ -5,15 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("*")
-            .exposedHeaders("*")
-            .allowCredentials(false)
-            .maxAge(3600);
-  }
-}
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//  @Override
+//  public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//            .allowedOrigins("*")
+//            .allowedMethods("*")
+//            .exposedHeaders("*")
+//            .allowCredentials(false)
+//            .maxAge(3600);
+//  }
+//}
