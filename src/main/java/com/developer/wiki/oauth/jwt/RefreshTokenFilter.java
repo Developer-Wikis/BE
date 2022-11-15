a1b41ae812ffe41458a9205011ba583d8b6ea5b4
jhdl0157 <jhdl0157@naver.com>
2022-11-15 21:43:56 +0900

    Feat : 토큰 갱신을 위한 필터 추가

---
+++ src/main/java/com/developer/wiki/oauth/jwt/RefreshTokenFilter.java
@@ -0,0 +1,173 @@
+package com.developer.wiki.oauth.jwt;
+
+import com.developer.wiki.oauth.TokenService;
+import com.developer.wiki.oauth.TokenType;
+import com.developer.wiki.oauth.User;
+import com.developer.wiki.oauth.UserRepository;
+import com.developer.wiki.oauth.exception.RefreshTokenException;
+import com.developer.wiki.oauth.util.JwtUtil;
+import com.developer.wiki.question.command.domain.EntityNotFoundException;
+import com.google.gson.Gson;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+
+@Log4j2
+@RequiredArgsConstructor
+public class RefreshTokenFilter  extends OncePerRequestFilter {
+
+    private final String refreshPath;
+
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+
+        if (!path.equals(refreshPath)) {
+            log.info("skip refresh token filter.....");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        log.info("Refresh Token Filter...run..............1");
+
+        //전송된 JSON에서 accessToken과 refreshToken을 얻어온다.
+        Map<String, String> tokens = parseRequestJSON(request);
+
+        String accessToken = tokens.get("accessToken");
+        String refreshToken = tokens.get("refreshToken");
+
+        log.info("accessToken: " + accessToken);
+        log.info("refreshToken: " + refreshToken);
+
+        try{
+            checkAccessToken(accessToken);
+        }catch(RefreshTokenException refreshTokenException){
+            refreshTokenException.sendResponseError(response);
+            return;
+        }
+
+        Map<String, Object> refreshClaims = null;
+
+        try {
+
+            refreshClaims = checkRefreshToken(refreshToken);
+            log.info(refreshClaims);
+
+        }catch(RefreshTokenException refreshTokenException){
+            refreshTokenException.sendResponseError(response);
+            return;
+        }
+
+        //Refresh Token의 유효시간이 얼마 남지 않은 경우
+        Integer exp = (Integer)refreshClaims.get("exp");
+
+        Date expTime = new Date(Instant.ofEpochMilli(exp).toEpochMilli() * 1000);
+
+        Date current = new Date(System.currentTimeMillis());
+
+        //만료 시간과 현재 시간의 간격 계산
+        //만일 3일 미만인 경우에는 Refresh Token도 다시 생성
+        long gapTime = (expTime.getTime() - current.getTime());
+
+        log.info("-----------------------------------------");
+        log.info("current: " + current);
+        log.info("expTime: " + expTime);
+        log.info("gap: " + gapTime );
+
+        String mid = (String)refreshClaims.get("email");
+
+        String accessTokenValue = jwtUtil.generateToken(Map.of("email", mid));
+
+        String refreshTokenValue = tokens.get("refreshToken");
+
+        //RefrshToken이 3일도 안남았다면
+        //TODO User안에 update Token해주기
+        if(gapTime < (60 *60 * 24 * 3* 1000L  ) ){
+            log.info("new Refresh Token required...  ");
+            refreshTokenValue = jwtUtil.generateRefreshToken();
+        }
+
+        log.info("Refresh Token result....................");
+        log.info("accessToken: " + accessTokenValue);
+        log.info("refreshToken: " + refreshTokenValue);
+
+        sendTokens(accessTokenValue, refreshTokenValue, response);
+
+
+    }
+
+    private Map<String,String> parseRequestJSON(HttpServletRequest request) {
+
+        try(Reader reader = new InputStreamReader(request.getInputStream())){
+
+            Gson gson = new Gson();
+
+            return gson.fromJson(reader, Map.class);
+
+        }catch(Exception e){
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+
+    private void checkAccessToken(String accessToken)throws RefreshTokenException {
+
+        try{
+            jwtUtil.validateToken(accessToken);
+        }catch (ExpiredJwtException expiredJwtException){
+            log.info("Access Token has expired");
+        }catch(Exception exception){
+            throw new RefreshTokenException(RefreshTokenException.ErrorCase.NO_ACCESS);
+        }
+    }
+
+    private Map<String, Object> checkRefreshToken(String refreshToken)throws RefreshTokenException{
+
+        try {
+            Map<String, Object> values = jwtUtil.validateToken(refreshToken);
+            User user=userRepository.findByRefreshToken(refreshToken).orElseThrow(EntityNotFoundException::new);
+            values.put("email",user.getEmail());
+            return values;
+
+        }catch(ExpiredJwtException expiredJwtException){
+            throw new RefreshTokenException(RefreshTokenException.ErrorCase.OLD_REFRESH);
+        }catch(Exception exception){
+            exception.printStackTrace();
+            throw new RefreshTokenException(RefreshTokenException.ErrorCase.NO_REFRESH);
+        }
+    }
+
+    private void sendTokens(String accessTokenValue, String refreshTokenValue, HttpServletResponse response) {
+
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        Gson gson = new Gson();
+
+        String jsonStr = gson.toJson(Map.of("accessToken", accessTokenValue,
+                "refreshToken", refreshTokenValue));
+
+        try {
+            response.getWriter().println(jsonStr);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
