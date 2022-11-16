46a262e9c6e5d7c7228459c853e5ece886f8fa99
dhkstnaos <dhkstnaos@gmail.com>
2022-11-16 15:01:26 +0900

    refactor: 질문 등록을 리팩토링하라.

--- src/main/java/com/developer/wiki/question/presentation/question/QuestionCreateController.java
+++ src/main/java/com/developer/wiki/question/presentation/question/QuestionCreateController.java
@@ -1,10 +1,14 @@
 package com.developer.wiki.question.presentation.question;
 
+import com.developer.wiki.common.exception.UnAuthorizedException;
+import com.developer.wiki.oauth.User;
 import com.developer.wiki.question.command.application.dto.CreateQuestionRequest;
 import com.developer.wiki.question.command.application.question.QuestionCreateService;
+import java.util.Objects;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +22,11 @@ public class QuestionCreateController {
   private final QuestionCreateService questionCreateService;
 
   @PostMapping
-  public ResponseEntity<Long> create(@RequestBody @Valid CreateQuestionRequest createQuestionRequest) {
+  public ResponseEntity<Long> create(@AuthenticationPrincipal User currentUser,
+      @RequestBody @Valid CreateQuestionRequest createQuestionRequest) {
+    if (Objects.isNull(currentUser)) {
+      throw new UnAuthorizedException("토큰이 필요합니다.");
+    }
     Long id = questionCreateService.create(createQuestionRequest);
     return ResponseEntity.ok(id);
   }
