007ea87e759b75c029c4c1c45553d97a1abfdc61
dhkstnaos <dhkstnaos@gmail.com>
2022-11-21 23:16:48 +0900

    refactor: 댓글 수정 서비스를 변경하라.

--- src/main/java/com/developer/wiki/question/command/application/comment/CommentModifyService.java
+++ src/main/java/com/developer/wiki/question/command/application/comment/CommentModifyService.java
@@ -1,5 +1,6 @@
 package com.developer.wiki.question.command.application.comment;
 
+import com.developer.wiki.common.exception.UnAuthorizedException;
 import com.developer.wiki.question.command.application.dto.ModifyCommentRequest;
 import com.developer.wiki.question.command.domain.Comment;
 import com.developer.wiki.question.command.domain.CommentRepository;
@@ -18,11 +19,17 @@ public class CommentModifyService {
 
   public void modify(Long id, ModifyCommentRequest modifyCommentRequest, Long userId) {
     Comment comment = commentRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-    if ((Objects.isNull(comment.getUserId()) && Objects.isNull(userId)) || (
-        !Objects.isNull(comment.getUserId()) && !Objects.isNull(userId))) {
-      comment.matchPassword(modifyCommentRequest.getPassword());
-      comment.changePassword(modifyCommentRequest.getPassword());
-      comment.changeContent(modifyCommentRequest.getContent());
+    checkAuthorization(userId, comment);
+    comment.matchPassword(modifyCommentRequest.getPassword());
+    comment.changePassword(modifyCommentRequest.getPassword());
+    comment.changeContent(modifyCommentRequest.getContent());
+  }
+
+  private void checkAuthorization(Long userId, Comment comment) {
+    if ((!Objects.isNull(comment.getUserId()) && Objects.isNull(userId)) || (
+        Objects.isNull(comment.getUserId()) && !Objects.isNull(userId))) {
+      throw new UnAuthorizedException("권한이 필요합니다.");
     }
   }
 }
+
