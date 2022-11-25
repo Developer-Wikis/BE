2faa0d5ff40e5cddea44b0e0037916dafc50b1e6
jhdl0157 <jhdl0157@naver.com>
2022-11-25 20:27:28 +0900

    Feat : 유저 닉네임 수정하는 api 추가

--- src/main/java/com/developer/wiki/oauth/controller/UserController.java
+++ src/main/java/com/developer/wiki/oauth/controller/UserController.java
@@ -3,18 +3,28 @@ package com.developer.wiki.oauth.controller;
 
 import com.developer.wiki.common.exception.BadRequestException;
 import com.developer.wiki.oauth.User;
+import com.developer.wiki.oauth.dto.ImageDto;
+import com.developer.wiki.oauth.dto.NicknameDto;
 import com.developer.wiki.oauth.dto.UserResponseDto;
 import com.developer.wiki.oauth.service.UserService;
+import com.developer.wiki.oauth.util.AwsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
     private final UserService userService;
+    private final AwsService awsService;
 
     @GetMapping
     public ResponseEntity<UserResponseDto> getUserInfo(@AuthenticationPrincipal User currentUser){
@@ -22,9 +32,37 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<String> deleteUSer(@AuthenticationPrincipal User currentUser, @PathVariable(name = "userId")Long userid){
+    public ResponseEntity<String> deleteUser(@AuthenticationPrincipal User currentUser, @PathVariable(name = "userId")Long userid){
         if(!currentUser.getId().equals(userid)) throw new BadRequestException("Not Match Userid");
         userService.deleteUser(currentUser.getId());
         return ResponseEntity.ok("delete");
+    }
+
+
+
+    @PostMapping("/nickname/{userId}")
+    public ResponseEntity<NicknameDto> changeUserName(@AuthenticationPrincipal User currentUser,
+                                                      @PathVariable(name = "userId")Long userId,
+                                                      @RequestBody @Valid NicknameDto nicknameDto){
+        if(!currentUser.getId().equals(userId)) throw new BadRequestException("Not Match Userid");
+        String newUserName=userService.updateUserName(nicknameDto.getUserName(),userId);
+        return ResponseEntity.ok(new NicknameDto(newUserName));
+    }
+
+    @PostMapping("/image/{userId}")
+    public ResponseEntity<String> changeImg(@AuthenticationPrincipal User currentUser, @RequestParam("image") MultipartFile file, @PathVariable Long userId) throws IOException {
+        if(!currentUser.getId().equals(userId)) throw new BadRequestException("Not Match Userid");
+        if(file.isEmpty()) throw new BadRequestException("파일은 Null이 될수 없습니다.");
+        String originalName=file.getOriginalFilename();
+        System.out.println("!!!!!!!!!!!!!!!오리지널"+originalName);
+        String[] name=originalName.split("\\.");
+        System.out.println(name.length);
+        final String ext = name[1];
+        final String saveFileName = getUuid() +"."+ ext;
+        System.out.println(saveFileName);
+        return ResponseEntity.ok(awsService.upload(saveFileName,file));
+    }
+    private static String getUuid() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
     }
 }
--- src/main/java/com/developer/wiki/oauth/service/UserService.java
+++ src/main/java/com/developer/wiki/oauth/service/UserService.java
@@ -1,10 +1,14 @@
 package com.developer.wiki.oauth.service;
 
+import com.developer.wiki.common.exception.BadRequestException;
+import com.developer.wiki.common.exception.NotFoundException;
+import com.developer.wiki.oauth.User;
 import com.developer.wiki.oauth.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +17,15 @@ public class UserService {
     @Transactional
     public void deleteUser(Long userId){
         userRepository.deleteById(userId);
+    }
+
+    @Transactional
+    public String updateUserName(String userName,Long userId){
+        if(userRepository.existsByName(userName)) throw new BadRequestException("닉네임 중복입니다.");
+        User user=userRepository.findById(userId)
+                .orElseThrow(()-> new NotFoundException("유저가 없습니다."));
+        user.changeUserName(userName);
+        return user.getName();
     }
 
 }
