cd948bc53677395a07ff31b7de308e96bc98cf27
jhdl0157 <jhdl0157@naver.com>
2022-11-29 16:24:47 +0900

    Feat : 코멘트 가져올때 question도 같이 가져오기 위해서 EntityGraph 설정(N+1)

--- src/main/java/com/developer/wiki/question/command/domain/CommentRepository.java
+++ src/main/java/com/developer/wiki/question/command/domain/CommentRepository.java
@@ -1,11 +1,15 @@
 package com.developer.wiki.question.command.domain;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
   List<Comment> findByQuestion_Id(Long questionId);
 
+
+  @EntityGraph(attributePaths = {"question"})
   List<Comment> findByUserId(Long userId);
 }
