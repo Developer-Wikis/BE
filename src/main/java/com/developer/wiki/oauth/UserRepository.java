1fe8816b8d26a0f330413f6cd22a321253c8c749
jhdl0157 <jhdl0157@naver.com>
2022-11-30 17:39:19 +0900

    Feat : user 조회시 작성한 댓글 갯수, 북마크한 갯수 추가

--- src/main/java/com/developer/wiki/oauth/UserRepository.java
+++ src/main/java/com/developer/wiki/oauth/UserRepository.java
@@ -1,6 +1,7 @@
 package com.developer.wiki.oauth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
