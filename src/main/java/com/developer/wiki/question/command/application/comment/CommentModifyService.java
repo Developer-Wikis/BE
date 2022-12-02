41ee002c5539540c2bbfd9a3187f343334ed78e3
dhkstnaos <dhkstnaos@gmail.com>
2022-12-02 19:57:59 +0900

    fix: 댓글 수정 로직 수정

--- src/main/java/com/developer/wiki/question/command/application/comment/CommentModifyService.java
+++ src/main/java/com/developer/wiki/question/command/application/comment/CommentModifyService.java
@@ -1,15 +1,13 @@
 package com.developer.wiki.question.command.application.comment;
 
-import com.developer.wiki.common.exception.UnAuthorizedException;
 import com.developer.wiki.question.command.application.dto.ModifyCommentRequest;
 import com.developer.wiki.question.command.domain.Comment;
 import com.developer.wiki.question.command.domain.CommentRepository;
 import com.developer.wiki.question.command.domain.EntityNotFoundException;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
 
 @Service
 @Transactional
@@ -20,17 +18,11 @@ public class CommentModifyService {
 
   public void modify(Long id, ModifyCommentRequest modifyCommentRequest, Long userId) {
     Comment comment = commentRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-    checkAuthorization(userId, comment);
-    comment.matchPassword(modifyCommentRequest.getPassword());
-    comment.changePassword(modifyCommentRequest.getPassword());
-    comment.changeContent(modifyCommentRequest.getContent());
-  }
-
-  private void checkAuthorization(Long userId, Comment comment) {
-    if ((!Objects.isNull(comment.getUserId()) && Objects.isNull(userId)) || (
-        Objects.isNull(comment.getUserId()) && !Objects.isNull(userId))) {
-      throw new UnAuthorizedException("권한이 필요합니다.");
+    if (Objects.isNull(userId)) {
+      comment.matchPassword(modifyCommentRequest.getPassword());
+      comment.changePassword(modifyCommentRequest.getPassword());
     }
+    comment.changeContent(modifyCommentRequest.getContent());
   }
 }
 
