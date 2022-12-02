ba1b76fde37404287771b4c719cd94bfcbfc26ef
dhkstnaos <dhkstnaos@gmail.com>
2022-12-02 19:57:52 +0900

    fix: 댓글 삭제 로직 수정

--- src/main/java/com/developer/wiki/question/command/application/comment/CommentDeleteService.java
+++ src/main/java/com/developer/wiki/question/command/application/comment/CommentDeleteService.java
@@ -1,6 +1,5 @@
 package com.developer.wiki.question.command.application.comment;
 
-import com.developer.wiki.common.exception.UnAuthorizedException;
 import com.developer.wiki.question.command.application.dto.PasswordRequest;
 import com.developer.wiki.question.command.domain.Comment;
 import com.developer.wiki.question.command.domain.CommentRepository;
@@ -8,8 +7,6 @@ import com.developer.wiki.question.command.domain.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
 
 @Service
 @Transactional
@@ -20,15 +17,7 @@ public class CommentDeleteService {
 
   public void delete(Long id, PasswordRequest passwordRequest, Long userId) {
     Comment comment = commentRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-    checkAuthorization(userId, comment);
     comment.matchPassword(passwordRequest.getPassword());
     commentRepository.delete(comment);
-  }
-
-  private void checkAuthorization(Long userId, Comment comment) {
-    if ((!Objects.isNull(comment.getUserId()) && Objects.isNull(userId)) || (
-        Objects.isNull(comment.getUserId()) && !Objects.isNull(userId))) {
-      throw new UnAuthorizedException("권한이 필요합니다.");
-    }
   }
 }
