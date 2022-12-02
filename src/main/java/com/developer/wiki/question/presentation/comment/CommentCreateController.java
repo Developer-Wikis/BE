9d6458f0f969fb0caed7a24c2f40ce0cb734bf25
dhkstnaos <dhkstnaos@gmail.com>
2022-12-02 21:04:14 +0900

    fix: comment 등록시 유효성 검사 제거

--- src/main/java/com/developer/wiki/question/presentation/comment/CommentCreateController.java
+++ src/main/java/com/developer/wiki/question/presentation/comment/CommentCreateController.java
@@ -3,13 +3,15 @@ package com.developer.wiki.question.presentation.comment;
 import com.developer.wiki.oauth.User;
 import com.developer.wiki.question.command.application.comment.CommentCreateService;
 import com.developer.wiki.question.command.application.dto.CreateCommentRequest;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.Objects;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/questions/{questionId}/comments")
@@ -21,7 +23,7 @@ public class CommentCreateController {
   @PostMapping
   public ResponseEntity<Long> create(@AuthenticationPrincipal User currentUser,
       @PathVariable Long questionId,
-      @RequestBody @Valid CreateCommentRequest createCommentRequest) {
+      @RequestBody CreateCommentRequest createCommentRequest) {
     Long userId = Objects.isNull(currentUser) ? null : currentUser.getId();
     Long id = commentCreateService.create(questionId, createCommentRequest,userId);
     return ResponseEntity.ok(id);
