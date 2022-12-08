0f9283215ebb9ca8511ad95b94b20e0fa1410a4e
dhkstnaos <dhkstnaos@gmail.com>
2022-12-08 23:24:21 +0900

    fix: 댓글 삭제 로직 수정

--- src/main/java/com/developer/wiki/question/command/application/comment/CommentDeleteService.java
+++ src/main/java/com/developer/wiki/question/command/application/comment/CommentDeleteService.java
@@ -4,6 +4,7 @@ import com.developer.wiki.question.command.application.dto.PasswordRequest;
 import com.developer.wiki.question.command.domain.Comment;
 import com.developer.wiki.question.command.domain.CommentRepository;
 import com.developer.wiki.question.command.domain.EntityNotFoundException;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,12 @@ public class CommentDeleteService {
 
   public void delete(Long id, PasswordRequest passwordRequest, Long userId) {
     Comment comment = commentRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-    comment.matchPassword(passwordRequest.getPassword());
-    commentRepository.delete(comment);
+    if (!Objects.isNull(userId)) {
+      commentRepository.delete(comment);
+    }
+    else if(Objects.isNull(userId) && Objects.nonNull(passwordRequest)){
+      comment.matchPassword(passwordRequest.getPassword());
+      commentRepository.delete(comment);
+    }
   }
 }
