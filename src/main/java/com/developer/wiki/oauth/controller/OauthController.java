13d2af7362575ea788c4f6623a4907d787079be0
jhdl0157 <jhdl0157@naver.com>
2022-12-21 23:26:04 +0900

    Feat : 깃헙 로그인 하드코딩

--- src/main/java/com/developer/wiki/oauth/controller/OauthController.java
+++ src/main/java/com/developer/wiki/oauth/controller/OauthController.java
@@ -36,53 +36,53 @@ public class OauthController {
     @Value("${custom.github.client-secret}")
     private  String GITHUB_CLIENT_SECRET;
 
-    @GetMapping()
-    public String Code(@RequestParam(value = "code")String code) throws JsonProcessingException {
-        //순서
-        // 1.코드와 리다이렉트 주소가 온다.
-        // 2.받은 정보로 토큰을 발급 받는다.
-        // 3. 토큰으로 사용자 정보를 요청한다.
-        System.out.println("코드 값임당 "+code);
-        String GOOGLE_TOKEN_REQUEST_URL="https://github.com/login/oauth/access_token";
-        RestTemplate restTemplate=new RestTemplate();
-        Map<String, Object> params = new HashMap<>();
-        // create headers
-        HttpHeaders headers1 = new HttpHeaders();
-// set `content-type` header
-        headers1.setContentType(MediaType.APPLICATION_JSON);
-// set `accept` header
-        headers1.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        params.put("code", code);
-        params.put("client_id", GITHUB_CLIENT_ID);
-        params.put("client_secret", GITHUB_CLIENT_SECRET);
-        params.put("redirect_uri", "http://localhost:8080/api/v1/oauth");
-        params.put("scope","user");
-        ResponseEntity<String> responseEntity;
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(params, headers1);
-        try {
-            responseEntity=restTemplate.postForEntity(GOOGLE_TOKEN_REQUEST_URL,entity,String.class);
-            System.out.println("엑세스 토큰 임당 "+responseEntity.getBody());
-        }catch (RestClientException e){
-            e.printStackTrace();
-            throw new BadRequestException(String.format("인가코드로 구글의 AccessToken을 발급하지 못했습니다. code : %s, redirectUrl : %s,  오류 내용 : %s",code,"redirectUrl",e.getMessage()));
-        }
-        GitHubOauthToken tokenTest=getAccessToken(responseEntity);
-        System.out.println("토큰 테스트 값 :"+tokenTest.getAccess_token());
-        String GOOGLE_USERINFO_REQUEST_URL="https://api.github.com/user";
-        //header에 accessToken을 담는다.
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization","Bearer "+tokenTest.getAccess_token());
-
-        //HttpEntity를 하나 생성해 헤더를 담아서 restTemplate으로 구글과 통신하게 된다.
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity(headers);
-        try {
-            ResponseEntity<String> response=restTemplate.exchange(GOOGLE_USERINFO_REQUEST_URL, HttpMethod.GET,request,String.class);
-            System.out.println("response.getBody() = " + response.getBody());
-            return response.getBody();
-        }catch (RestClientException e){
-            throw  new BadRequestException("구글 AccessToken을 으로 사용자 정보를 가져오지 못했습니다.");
-        }
-    }
+//    @GetMapping()
+//    public String Code(@RequestParam(value = "code")String code) throws JsonProcessingException {
+//        //순서
+//        // 1.코드와 리다이렉트 주소가 온다.
+//        // 2.받은 정보로 토큰을 발급 받는다.
+//        // 3. 토큰으로 사용자 정보를 요청한다.
+//        System.out.println("코드 값임당 "+code);
+//        String GOOGLE_TOKEN_REQUEST_URL="https://github.com/login/oauth/access_token";
+//        RestTemplate restTemplate=new RestTemplate();
+//        Map<String, Object> params = new HashMap<>();
+//        // create headers
+//        HttpHeaders headers1 = new HttpHeaders();
+//// set `content-type` header
+//        headers1.setContentType(MediaType.APPLICATION_JSON);
+//// set `accept` header
+//        headers1.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+//        params.put("code", code);
+//        params.put("client_id", GITHUB_CLIENT_ID);
+//        params.put("client_secret", GITHUB_CLIENT_SECRET);
+//        params.put("redirect_uri", "http://localhost:8080/api/v1/oauth");
+//        params.put("scope","user");
+//        ResponseEntity<String> responseEntity;
+//        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(params, headers1);
+//        try {
+//            responseEntity=restTemplate.postForEntity(GOOGLE_TOKEN_REQUEST_URL,entity,String.class);
+//            System.out.println("엑세스 토큰 임당 "+responseEntity.getBody());
+//        }catch (RestClientException e){
+//            e.printStackTrace();
+//            throw new BadRequestException(String.format("인가코드로 구글의 AccessToken을 발급하지 못했습니다. code : %s, redirectUrl : %s,  오류 내용 : %s",code,"redirectUrl",e.getMessage()));
+//        }
+//        GitHubOauthToken tokenTest=getAccessToken(responseEntity);
+//        System.out.println("토큰 테스트 값 :"+tokenTest.getAccess_token());
+//        String GOOGLE_USERINFO_REQUEST_URL="https://api.github.com/user";
+//        //header에 accessToken을 담는다.
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Authorization","Bearer "+tokenTest.getAccess_token());
+//
+//        //HttpEntity를 하나 생성해 헤더를 담아서 restTemplate으로 구글과 통신하게 된다.
+//        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity(headers);
+//        try {
+//            ResponseEntity<String> response=restTemplate.exchange(GOOGLE_USERINFO_REQUEST_URL, HttpMethod.GET,request,String.class);
+//            System.out.println("response.getBody() = " + response.getBody());
+//            return response.getBody();
+//        }catch (RestClientException e){
+//            throw  new BadRequestException("구글 AccessToken을 으로 사용자 정보를 가져오지 못했습니다.");
+//        }
+//    }
     private GitHubOauthToken getAccessToken(ResponseEntity<String> response) throws JsonProcessingException {
         System.out.println("response.getBody() = " + response.getBody());
         GitHubOauthToken gitHubOauthToken= objectMapper.readValue(response.getBody(),GitHubOauthToken.class);
@@ -105,7 +105,44 @@ public class OauthController {
     public ResponseEntity<String> getGitHubUserInfo(@RequestParam("code") String code,@RequestParam("redirectUrl")String redirectUrl) throws IOException {
         System.out.println(">> 소셜 로그인 API 서버로부터 받은 code :"+ code);
         System.out.println(">> 소셜 로그인 API 서버로부터 받은 url :"+ redirectUrl);
-        //GoogleResponseDto GoogleUser = oauthService.oAuthLogin(code,redirectUrl);
-        return new ResponseEntity<>(code, HttpStatus.OK);
+        String GOOGLE_TOKEN_REQUEST_URL="https://github.com/login/oauth/access_token";
+        RestTemplate restTemplate=new RestTemplate();
+        Map<String, Object> params = new HashMap<>();
+        // create headers
+        HttpHeaders headers1 = new HttpHeaders();
+// set `content-type` header
+        headers1.setContentType(MediaType.APPLICATION_JSON);
+// set `accept` header
+        headers1.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        params.put("code", code);
+        params.put("client_id", GITHUB_CLIENT_ID);
+        params.put("client_secret", GITHUB_CLIENT_SECRET);
+        params.put("redirect_uri", redirectUrl);
+        params.put("scope","user");
+        ResponseEntity<String> responseEntity;
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(params, headers1);
+        try {
+            responseEntity=restTemplate.postForEntity(GOOGLE_TOKEN_REQUEST_URL,entity,String.class);
+            System.out.println("엑세스 토큰 임당 "+responseEntity.getBody());
+        }catch (RestClientException e){
+            e.printStackTrace();
+            throw new BadRequestException(String.format("인가코드로 구글의 AccessToken을 발급하지 못했습니다. code : %s, redirectUrl : %s,  오류 내용 : %s",code,"redirectUrl",e.getMessage()));
+        }
+        GitHubOauthToken tokenTest=getAccessToken(responseEntity);
+        System.out.println("토큰 테스트 값 :"+tokenTest.getAccess_token());
+        String GOOGLE_USERINFO_REQUEST_URL="https://api.github.com/user";
+        //header에 accessToken을 담는다.
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization","Bearer "+tokenTest.getAccess_token());
+
+        //HttpEntity를 하나 생성해 헤더를 담아서 restTemplate으로 구글과 통신하게 된다.
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity(headers);
+        try {
+            ResponseEntity<String> response=restTemplate.exchange(GOOGLE_USERINFO_REQUEST_URL, HttpMethod.GET,request,String.class);
+            System.out.println("response.getBody() = " + response.getBody());
+            return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+        }catch (RestClientException e){
+            throw  new BadRequestException("구글 AccessToken을 으로 사용자 정보를 가져오지 못했습니다.");
+        }
     }
 }
